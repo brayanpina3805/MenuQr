3a9c79addcee0d5c4beeb716097dd137e5bdba85
brayanpina3805 <af339150@gmail.com>
2025-01-10 20:59:24 -0600

    Corregimos errores de login y agregamos alerta

--- src/main/java/com/umb/cafeteria/Contollers/InicioController.java
+++ src/main/java/com/umb/cafeteria/Contollers/InicioController.java
@@ -1,6 +1,8 @@
 package com.umb.cafeteria.Contollers;
 
+import com.umb.cafeteria.Entity.Usuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -17,4 +19,9 @@ public class InicioController {
         return "login";
     }
 
+       @GetMapping("/auth/login")
+    public String login(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "login";
+    }
 }
